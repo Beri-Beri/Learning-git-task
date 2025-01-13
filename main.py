78015417dc62fe12e0cfe9f6b2665d5465bd5655
Bernadetta Majta <bernadettamajta@gmail.com>
2025-01-13 10:57:11 +0100

    Print sum of products in dict

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 shopping_list = {
     "piekarnia": ["chleb", "bułki", "pączek"],
-    "warzywniak": ["marchew", "seler", "rukola"]
+    "warzywniak": ["marchew", "seler", "rukola"]}
 
-    print("Lista zakupów")
+print("Lista zakupów")
 cap_shopping_list = {shop.capitalize(): [product.capitalize() for product in products] for shop, products in shopping_list.items()}
 for shop, product in cap_shopping_list.items():
     print(f"Idę do {shop}, kupuję tu następujące rzeczy: {product}")
+
+product_no = sum([len(shopping_list[product]) for product in shopping_list if isinstance(shopping_list[product], list)])
+print(f"W sumie kupuję {product_no} produktów")
